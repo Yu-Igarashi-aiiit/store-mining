d51d5f9adeb339ea80970eae9bd3cc6cfc32b948
Yu-Igarashi-aiiit <39993912+Yu-Igarashi-aiiit@users.noreply.github.com>
2018-06-19 12:14:47 +0900

    upload mining_test.py(var_2.0)
    complete web scraping & feature extraction function.

--- mining_test.py
+++ mining_test.py
@@ -3,10 +3,11 @@ from janome.tokenizer import Tokenizer
 from janome.analyzer import Analyzer
 from janome.tokenfilter import POSKeepFilter
 from janome.charfilter import UnicodeNormalizeCharFilter,RegexReplaceCharFilter
-# import requests #HTML解析のフレームワーク
-# from bs4 import BeautifulSoup
+import requests #HTML解析のフレームワーク
+from bs4 import BeautifulSoup
 
-def store_mining(description:str,reject_symbol=[("[,\.\(\)\{\}\[\]\<\>\○]"," ")]):
+#文章中から名詞をリストで取得する
+def get_keyword(description:str,reject_symbol=[("[,\.\(\)\{\}\[\]\<\>\○]"," ")]):
     #analyzerの前処理フィルタ生成
     char_filters = [UnicodeNormalizeCharFilter()]
     #reject_symbolは正規化し名詞判定されないようにする
@@ -14,7 +15,7 @@ def store_mining(description:str,reject_symbol=[("[,\.\(\)\{\}\[\]\<\>\○]"," "
         char_filters.append(RegexReplaceCharFilter(*i))
 
     #tokenizerの初期化、tokenizerインスタンスは使い回す、ループの外で呼び出し初期化
-    tokenizer = Tokenizer()
+    tokenizer = Tokenizer(mmap=True)
     #品詞取得フィルタ
     token_filters = [POSKeepFilter(['名詞'])]
     #各種フィルタ設定でanalyze
@@ -24,5 +25,26 @@ def store_mining(description:str,reject_symbol=[("[,\.\(\)\{\}\[\]\<\>\○]"," "
     # for token in analyzer.analyze(description):
     #     print(token.surface)
 
-description='【主な機能と使い方】＜1.今すぐ呼ぶ＞乗車場所は地図上で簡単に設定できます。その後、必要な台数や降車場所(任意)、Google Payなどの決済方法(任意)を設定すれば配車完了です。アプリには到着までの待ち時間 (あと約○○分)と車両番号が表示されるので、乗るタクシーがすぐに分かり安心です。'
-store_mining(description)
+#対象を示す表現をリストで取得する
+def get_attribute(word_list):
+    list=[]
+    word_list.sort()
+    for i in range(0,len(word_list)):
+        if ((i == 0) | ((word_list[i] != word_list[i-1])&(i>=1))):
+            if((len(word_list[i]) >= 2) & (word_list.count(word_list[i]) >= 2)):
+                #print("{h1} = {h2}".format(h1=word_list[i], h2=word_list.count(word_list[i])))
+                list += [wordlist[i]]
+    return list
+
+
+description = requests.get("https://ja.wikipedia.org/wiki/マイメロディ")
+content = description.content
+soup = BeautifulSoup(content,'html.parser')
+article = soup.find_all("p")
+#find_allはリスト形式を返すため、リストを文字列に変換する
+bun=""
+for i in range(0,len(article)):
+    bun += article[i].getText()
+wordlist = get_keyword(bun)
+AttributeList = get_attribute(wordlist)
+print(AttributeList)
